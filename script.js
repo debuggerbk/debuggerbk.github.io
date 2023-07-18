eec4c33ac6ea8f052cd0ff4c70841408cf913fdd
Yasir Eymen Kayabaşı <93262823+yaso09@users.noreply.github.com>
2023-07-18 19:42:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector("#debug").addEventListener("click", function() {
+    const wave = document.querySelector("#wave");
+    if (wave.paused) {
+        wave.play();
+        document.querySelector("#debug").value = "Böcek Ayıklamayı Durdur";
+    }
+    else {
+        wave.pause();
+        document.querySelector("#debug").value = "Böcek Ayıkla";
+    }
+});
+document.querySelector("#wave").onended = function() {
+    wave.play();
+}
